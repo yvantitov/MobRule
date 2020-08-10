19a4d5963652feaeeb61aa3866a0f1d39b43cbd6
yvantitov <yvantitov@gmail.com>
2020-08-09 22:05:55 -0700

    initial commit part 2 electric boogaloo

---
+++ src/main/java/com/github/yvantitov/mobrule/MobRule.java
@@ -0,0 +1,10 @@
+package com.github.yvantitov.mobrule;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MobRule extends JavaPlugin {
+    @Override
+    public void onEnable() {
+
+    }
+}
